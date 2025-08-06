b872cd9721e304f2904b66f63ad0623b7f31774e
readydev-one <toricamstudios@gmail.com>
2025-08-06 11:31:52 -0400

    implement login service for backend #01

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -56,3 +56,56 @@ router.post('/register', async (req, res) => {
          return res.status(500).send('Internal server error');
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection("users");
+
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+
+        // Task 7: Send appropriate message if the user is not found
+        if (!theUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        if (theUser) {
+
+            // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if(!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            //continue other tasks
+            // Task 5: Fetch user details from the database
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+
+            // Task 6: Create JWT authentication if passwords match with user._id as payload
+            const payload = {
+            user: {
+                id: theUser._id.toString(),
+            },
+        };
+        jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+
+        logger.info('Login successful');
+        return res.json({ authtoken, userName, userEmail });
+
+        } else {
+        logger.error('User not found');
+        return res.status(404).json({ error: 'User not found' });
+        }
+
+
+    } catch (e) {
+        logger.error('Internal server error', e);
+        return res.status(500).send('Internal server error');
+    }
+});
